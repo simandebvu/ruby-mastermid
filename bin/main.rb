acfd0acf92ff50e81c68e318c84f5f825df6de32
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-11 22:33:47 +0200

    Resolved Rubocop Offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,17 @@
 puts 'Welcome to MasterMind!'
 
-puts 'What would you like to be ? The Coder (Have me guess the colours) or the Decoder (Having you guess the colors)? Choose wisely- because I never lose ;)'
+puts 'What would you like to be ? The Coder (Have me guess the colours) or the Decoder (Having you guess the colors)? '
+puts 'Choose wisely- because I never lose ;)'
 
 puts 'D - Decoder, C - Coder'
 choice = gets.strip.downcase
 
 case choice
-when "d"
-    puts "Welcome Decoder !"
-    puts "Guess #1 : "
-when "c"
-    puts "Welcome Coder ! "
+when 'd'
+  puts 'Welcome Decoder !'
+  puts 'Guess #1 : '
+when 'c'
+  puts 'Welcome Coder ! '
 else
-    puts "Type d for decoder or c for coder. Nothing else!"
+  puts 'Type d for decoder or c for coder. Nothing else!'
 end
